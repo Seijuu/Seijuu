1e28e1078b39a34bc5fab8899bdea568cdf667e9
seijuuboy <95195850+seijuuboy@users.noreply.github.com>
2021-11-28 22:43:34 -0300

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar -c ~/.config/polybar/config example &
+polybar -c ~/.config/polybar/config seijuu &
