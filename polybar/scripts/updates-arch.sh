509fc668255d855ebca18f37aa6a91b934d2701f
seijuuboy <95195850+seijuuboy@users.noreply.github.com>
2021-12-17 20:19:41 -0300

    Update updates-arch.sh

--- polybar/scripts/updates-arch.sh
+++ polybar/scripts/updates-arch.sh
@@ -1,7 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-arch_updates=$(checkupdates 2>&1)
-aur_updates=$(yay --aur -Qu 2>/dev/null | wc -l)
-arch_updates=$(echo "$arch_updates" | wc -l)
-updates=$((arch_updates + aur_updates))
-echo "$updates"
+if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+    updates_arch=0
+fi
+
+if ! updates_aur=$(yay -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(paru -Qum 2> /dev/null | wc -l); then
+# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
+# if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+# if ! updates_aur=$(pikaur -Qua 2> /dev/null | wc -l); then
+# if ! updates_aur=$(rua upgrade --printonly 2> /dev/null | wc -l); then
+    updates_aur=0
+fi
+
+updates=$((updates_arch + updates_aur))
+
+if [ "$updates" -gt 0 ]; then
+    echo "$updates"
+else
+    echo ""
+fi
